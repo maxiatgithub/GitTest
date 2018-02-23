f77cf029095315da9d2bbea9c591dfd71693c8e1
Maximilian M <maxserver@outlook.de>
2018-02-23 01:29:32 +0100

    neue Ausgabe

--- GitTest3/Program.cs
+++ GitTest3/Program.cs
@@ -7,6 +7,8 @@ namespace GitTest3
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("jetzt");
+            Console.Read();
         }
     }
 }
